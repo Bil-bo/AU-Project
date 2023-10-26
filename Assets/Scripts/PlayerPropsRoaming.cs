6eee2d7d1c79a9341a1bdd0a478223cab14786da
Bhavan <backupbhavan@gmail.com>
2023-10-26 21:39:53 +0100

    New functions, win text

--- Assets/Scripts/PlayerPropsRoaming.cs
+++ Assets/Scripts/PlayerPropsRoaming.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 using UnityEngine;
 using System;
@@ -12,17 +13,30 @@ public class PlayerPropsRoaming : MonoBehaviour
 
     public float _speed;
     public Vector2 moveValue;
+    
+    private Vector3 initialPlayerPosition;
     private CharacterController charcon;
     private bool charIsGrounded;
     public MainGameManager gameManager;
 
     private int collectedPickups = 0;
+    public int maxPickups = 3;
+    private bool hasWon = false;
+
+    private List<Vector3> initialEnemyPositions = new List<Vector3>();
 
     // Start is called before the first frame update
     void Start()
     {
         charcon = GetComponent<CharacterController>();
         collectedPickups = PlayerPrefs.GetInt("PickupsCollected");
+        initialPlayerPosition = transform.position;
+
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject enemy in enemies)
+        {
+            initialEnemyPositions.Add(enemy.transform.position);
+        }
         UpdatePickupText();
     }
 
@@ -69,6 +83,11 @@ public class PlayerPropsRoaming : MonoBehaviour
                 collectedPickups++;
                 PlayerPrefs.SetInt("PickupsCollected", collectedPickups);
                 UpdatePickupText();
+
+                if(collectedPickups >= maxPickups){
+                    hasWon = true;
+                    DisplayWinMessage();
+                }
                 break;
             case "Enemy":
                 other.gameObject.SetActive(false);
@@ -79,10 +98,48 @@ public class PlayerPropsRoaming : MonoBehaviour
 
     private void UpdatePickupText()
     {
-        TextMeshProUGUI pickupText = FindFirstObjectByType<TextMeshProUGUI>();
+        TextMeshProUGUI pickupText = GameObject.FindGameObjectWithTag("PickupText")?.GetComponent<TextMeshProUGUI>();
 
         if(pickupText != null){
             pickupText.text = "Pickups collected: " + collectedPickups;
         }  
+    }
+
+    private void DisplayWinMessage()
+    {
+        
+
+        if(hasWon){
+            TextMeshProUGUI winText = GameObject.FindGameObjectWithTag("WinText")?.GetComponent<TextMeshProUGUI>();
+
+            if(winText != null)
+            {
+                winText.text = "You won!";
+
+                transform.position = initialPlayerPosition;
+                collectedPickups = 0;
+                PlayerPrefs.SetInt("PickupsCollected", 0);
+                RespawnEnemies();
+
+                SceneManager.LoadScene("Menu");
+
+
+                
+            }
+        }
+    }
+
+    private void RespawnEnemies()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+    // Iterate through the enemy game objects and set their positions to their initial spawn positions.
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            if (i < initialEnemyPositions.Count)
+            {
+                enemies[i].transform.position = initialEnemyPositions[i];
+            }
+        }
     }
 }
