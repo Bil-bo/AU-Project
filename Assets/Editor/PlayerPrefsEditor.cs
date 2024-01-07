6c5916ec27385f42920ce9594af2b46db10e9c61
Joshua Seabrook <joshua.d.seabrook@gmail.com>
2024-01-07 20:22:55 +0000

    FEAT: Add PrintAllPlayerPrefs() editor function
    +Added PlayerPrefsEditor.cs script
    +New function can be accessed my going to Tools -> Print all playerprefs in the editor topbar

---
+++ Assets/Editor/PlayerPrefsEditor.cs
@@ -0,0 +1,40 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using Microsoft.Win32;
+
+public static class PlayerPrefsUtility
+{
+    public static void PrintAllPlayerPrefs()
+    {
+        
+        string registryPath = "Software\\Unity\\UnityEditor\\" + Application.companyName + "\\" + Application.productName;
+        RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(registryPath);
+
+        if (registryKey != null)
+        {
+            string allPrefs = "ALL PLAYERPREFS:\n"; // Initialize an empty string to store all preferences
+            foreach (string key in registryKey.GetValueNames())
+            {
+                string value = registryKey.GetValue(key).ToString();
+                allPrefs += key + " : " + value + "\n"; // Append each key-value pair to the string
+            }
+
+            Debug.Log(allPrefs); // Log all PlayerPrefs at once
+        }
+        else
+        {
+            Debug.Log("No PlayerPrefs found for this application.");
+        }
+    }
+}
+
+public class PlayerPrefsEditor : EditorWindow
+{
+    [MenuItem("Tools/Print All PlayerPrefs")]
+    private static void PrintPlayerPrefs()
+    {
+        PlayerPrefsUtility.PrintAllPlayerPrefs();
+    }
+}
+#endif
